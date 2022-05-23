18b8e85a365c2919097c43f0958df878b5045bf7
jo-ho <joshuagjho@hotmail.com>
2022-05-22 23:46:10 -0700

    Fix counter

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,34 @@
-import React, {useState} from "react";
+import React from "react";
 
 
 
-
+const tries = 3
 
   
   
   class App extends React.Component{
-    
-    handleInput(e){
-      if(e.target.id === this.state.language){
-        console.log("win")
-      }
-      else if(this.state.counter <= 2){
-        console.log("lose try again")
-        this.setState({counter: this.state.counter+1})
-      }
-      else{
-        console.log("loser")
-      }
-    }    
+
     constructor(props) {
       super(props);
       this.state = {word: 'cheese',translated: 'fromage', language: 'French', counter: 0};
       
     }
+
+    
+    handleInput(e){
+		
+		  if(e.target.id === this.state.language){
+			console.log("win")
+		  }
+		  else if(this.state.counter < tries){
+			this.setState({counter: this.state.counter+1})
+
+			console.log("lose try again")
+		  }
+		  else{
+			console.log("loser")
+		  }
+		}    
     render(){
       return(
         <>
@@ -32,20 +36,20 @@ import React, {useState} from "react";
         <center>{this.state.translated}</center>
 
   
-        <center class="btngroup1">
+        <center className="btngroup1">
 
           <button id = "Czech" onClick={e => this.handleInput(e)}>Czech</button>
           <button id = "French" onClick={e => this.handleInput(e)}>French</button>
           <button id = "German" onClick={e => this.handleInput(e)}>German</button>
 
         </center>
-        <center class="btngroup2">
+        <center className="btngroup2">
           <button id = "Italian" onClick={e => this.handleInput(e)}>Italian</button>
           <button id = "Norwegian" onClick={e => this.handleInput(e)}>Norwegian</button>
           <button id = "Polish" onClick={e => this.handleInput(e)}>Polish</button>
 
         </center>
-        <center class="btngroup3">
+        <center className="btngroup3">
           <button id = "Spanish" onClick={e => this.handleInput(e)}>Spanish</button>
           <button id = "Swedish" onClick={e => this.handleInput(e)}>Swedish</button>
           <button id = "Turkisk" onClick={e => this.handleInput(e)}>Turkish</button>
