3d3ee052d1a9d9eb4985be0c406e0b649c9d911b
jo-ho <joshuagjho@hotmail.com>
2022-06-12 18:24:25 -0700

    Deploy app to Heroku

--- server/index.js
+++ server/index.js
@@ -11,14 +11,15 @@ var languages = ['cs', 'fr', 'de', 'it', 'no', 'pl', 'es', 'sv', 'tr']
 
 app.use(express.static(__dirname + '../client/public'));
 
+var data = fs.readFileSync(path.join('server','data', 'wordle-answers-alphabetical.txt'), 'utf8');
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
 
 app.get("/word", async (req, res) => {
-	var data = fs.readFileSync(path.join('server','data', 'wordle-answers-alphabetical.txt'), 'utf8');
-    var words = data.toString().split("\n");
+
+  var words = data.toString().split("\n");
 
 	var word = words[Math.floor(Math.random()*words.length)];
 	var language = languages[Math.floor(Math.random()*languages.length)]
